c02ea34de223a45999d0ac7351b03c0d79d182ab
Marco Antonio Hernandez Alba <marco@grupohernandezalba.com>
2024-05-13 21:20:52 -0600

    proyecto10

--- proyecto8/js/index.js
+++ proyecto8/js/index.js
@@ -1,28 +1,56 @@
     let formulario = document.getElementById('formulario');
+    let formularioEditar = document.getElementById('formularioEditar');
     let nombre = document.getElementById('nombre');
     let fecha = document.getElementById('fecha');
+    let urlvideo = document.getElementById('urlvideo');
     let descripcion = document.getElementById('descripcion');
+    let idTarea = document.getElementById('idTarea');
 
-    let tareas = [];
+    let tareas = [
+    {   
+        nombre : "Josafat" ,
+        fecha : "2023-12-31", 
+        descripcion : "Hola mundo" ,
+        cancion: "cancion.mp4"
+    },
+    {   
+        nombre : "Abraham" ,
+        fecha : "2024-10-21", 
+        descripcion : "Hola mundo1",
+        cancion: "cancion.mp4"
+    } ,
+    { 
+        nombre : "Fer" ,
+        fecha : "2025-12-27", 
+        descripcion : "Hola mundo2",
+        cancion: "cancion.mp4"
+    }];
+
     let listaTareas = document.getElementById("listaTareas");
     let btnGuardar = document.getElementById('btnGuardar');
+    let btnGuardarEditar = document.getElementById('btnGuardarEditar');
 
     function mostrarTareas(){
         listaTareas.innerHTML = "";
         tareas.forEach((tarea,indice) => {
             listaTareas.innerHTML += `
             <div class='row'>
-                <div class='col-3 border p-3'>
+                <div class='col-2 border p-3'>
+                <video width="100%" controls>
+                    <source src="${ tarea.cancion }" type="video/mp4">
+                </video>
+                </div>
+                <div class='col-1 border p-3'>
                     <strong>${ tarea.nombre }</strong>
                 </div>
-                <div class='col-3 border p-3'>
+                <div class='col-1 border p-3'>
                     <strong>${ tarea.fecha }</strong>
                 </div>
                 <div class='col-4 border p-3'>
                     <strong>${ tarea.descripcion }</strong>
                 </div>
                 <div class='col-1 border p-3 text-center'>
-                    <button class='btn btn-success'>Editar</button>
+                    <button class='btn btn-success' data-bs-toggle="modal" data-bs-target="#editarTarea" onclick="editarTarea(${ indice })">Editar</button>
                 </div>
                 <div class='col-1 border p-3 text-center'>
                     <button class='btn btn-danger' onClick="borrarTarea(${ indice })">Borrar</button>
@@ -34,11 +62,29 @@
 
     mostrarTareas();
 
+    let editarTarea = (indice) => {
+        nombreEditar.value = tareas[indice].nombre;
+        fechaEditar.value = tareas[indice].fecha;
+        descripcionEditar.value = tareas[indice].descripcion;
+        idTarea.value = indice;
+    }
+
+    formularioEditar.addEventListener("submit", (e)=>{
+        e.preventDefault();
+        let indice = idTarea.value;
+        tareas[indice].nombre = nombreEditar.value;
+        tareas[indice].fecha = fechaEditar.value;
+        tareas[indice].descripcion = descripcionEditar.value;
+        mostrarTareas();
+        cerrarModalEditar();
+    });
+
     let agregarDatos = ()=> {
         let datos = {
             nombre: nombre.value,
             fecha: fecha.value,
-            descripcion: descripcion.value
+            descripcion: descripcion.value,
+            cancion: urlvideo.value
         }
         tareas.push(datos);
         mostrarTareas();
@@ -47,6 +93,11 @@
     let cerrarModal = ()=> {
         btnGuardar.setAttribute("data-bs-dismiss","modal");
         btnGuardar.click();
+    }
+
+    let cerrarModalEditar = ()=> {
+        btnGuardarEditar.setAttribute("data-bs-dismiss","modal");
+        btnGuardarEditar.click();
     }
 
     let borrarTarea = (indice)=> {
